cc5e687dfa17b9c08ebeb9336da66a542fb0749d
mattigu <gulinski.mateusz1@gmail.com>
2025-07-01 13:56:16 +0200

    Improved input parsing and added a PlayerKind enum

--- src/player.rs
+++ src/player.rs
@@ -3,50 +3,74 @@ use std::io;
 use crate::board::Board;
 
 pub trait Player {
-    fn new(symbol: char) -> Self;
-    fn get_move(&self, board: &Board) -> u64;
+    fn get_move(&self, board: Board) -> Option<u64>;
     fn get_symbol(&self) -> char;
 }
 
-struct Human {
+pub enum PlayerKind {
+    Human(Human),
+    // Random(RandomAI),
+    // Minimax(Minimax),
+}
+
+impl Player for PlayerKind {
+    fn get_symbol(&self) -> char {
+        match self {
+            PlayerKind::Human(p) => p.get_symbol(),
+            // PlayerKind::Random(p) => p.get_symbol(),
+        }
+    }
+
+    fn get_move(&self, board: Board) -> Option<u64> {
+        match self {
+            PlayerKind::Human(p) => p.get_move(board),
+            // PlayerKind::Random(p) => p.get_move(board),
+        }
+    }
+}
+
+pub struct Human {
     symbol: char,
 }
 
 impl Human {
-    pub fn parse_input(input: &str) -> Option<(u8, u8)> {
+    pub const fn new(symbol: char) -> Self {
+        Self { symbol }
+    }
+
+    pub fn parse_input(input: &str) -> Option<u64> {
         let mut chars = input.chars();
         let file = chars.next()?;
         let rank = chars.next()?;
         if chars.next().is_some() {
             return None;
         }
-        Some((rank as u8 - b'1', file as u8 - b'a'))
+        let mv = Board::get_move(b'8' - rank as u8, file as u8 - b'a');
+        Some(mv)
     }
 }
 
 impl Player for Human {
-    fn new(symbol: char) -> Human {
-        Human { symbol }
-    }
-
     fn get_symbol(&self) -> char {
         self.symbol
     }
 
-    fn get_move(&self, board: &Board) -> u64 {
+    fn get_move(&self, board: Board) -> Option<u64> {
+        if board.num_moves(self.get_symbol() == 'x') == 0 {
+            return None;
+        }
         let mut input = String::new();
-        println!("Enter move:");
+        println!("Enter move");
         loop {
+            input.clear();
             let _ = io::stdin().read_line(&mut input);
-            Human::parse_input(&input).and_then(|(row, col)| {
-                let mv = Board::get_move(row, col);
-                if board.is_legal(mv, self.get_symbol() == 'x') {
-                    Some(mv)
-                } else {
-                    print!("Illegal move.");
-                    None
-                }
-            });
+
+            if let Some(mv) = Human::parse_input(input.trim())
+                && board.is_legal(mv, self.get_symbol() == 'x')
+            {
+                return Some(mv);
+            }
+            println!("Illegal move.");
         }
     }
 }
