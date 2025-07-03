a78018ce732b0e05934304354b38856b7fd01c8c
mattigu <gulinski.mateusz1@gmail.com>
2025-07-03 14:05:57 +0200

    Improved print formatting for benchmarks and added column headers

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -8,6 +8,8 @@ use othello::game::Outcome;
 use othello::player::Minimax;
 use othello::player::Player;
 use othello::player::RandomAI;
+use othello::utils::ansi_for;
+use othello::utils::color;
 pub struct Stat {
     x_wins: u32,
     draws: u32,
@@ -35,19 +37,42 @@ impl Stat {
             description,
         }
     }
+
+    fn print_header() {
+        println!(
+            "{:<18} | {:^22} | {:^4} | {:^7} | {:^5}",
+            "Description", "results", "runs", "total", "avg"
+        );
+    }
+
+    fn format_time(&self, duration: Duration) -> String {
+        let time = duration.as_nanos();
+        if time < 1000 {
+            format!("{time} ns")
+        } else if time < 1_000_000 {
+            format!("{:.2} μs", duration.as_micros())
+        } else if time < 1_000_000_000 {
+            format!("{:.2} ms", duration.as_millis())
+        } else {
+            format!("{:.2} s", duration.as_secs_f64())
+        }
+    }
 }
 
 impl fmt::Display for Stat {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{:<18} | X {:>4}  O {:>4}  D {:>4} | {:>4} g | {:>.3} s",
+            "{:<18} | {} {:>4}  {} {:>4}  D {:>4} | {:>4} | {:>7} | {:>5}",
             self.description,
+            color("X", ansi_for('x')),
             self.x_wins,
+            color("O", ansi_for('o')),
             self.o_wins,
             self.draws,
             self.iterations,
-            self.duration.as_secs_f64(),
+            self.format_time(self.duration),
+            self.format_time(self.duration / self.iterations),
         )
     }
 }
@@ -86,14 +111,19 @@ pub fn run_bench<P1: Player, P2: Player>(
 }
 
 fn main() {
+    Stat::print_header();
+    //
     let random_o = RandomAI::new('o');
     let random_x = RandomAI::new('x');
 
     let stat = run_bench(random_x, random_o, 1000, "Random vs random");
     println!("{stat}");
+    //
 
     let mini_x_5 = Minimax::new('x', 5, SimpleEval {});
     let random_o = RandomAI::new('o');
     let stat = run_bench(mini_x_5, random_o, 100, "Mini 5s vs random");
     println!("{stat}");
+
+    //
 }
