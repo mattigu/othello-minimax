e10898abb7dc7cd19f159ff8799644f1b257a1f0
mattigu <gulinski.mateusz1@gmail.com>
2025-07-03 22:59:23 +0200

    Moved terminal clear and added new options for the game.

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ fn main() {
     println!("2 - player vs random");
     println!("3 - random vs random");
     println!("4 - player vs best algorithm");
+    println!("5 - random vs best algorithm");
 
     let mut input = String::new();
     let _ = io::stdin().read_line(&mut input);
@@ -30,6 +31,7 @@ fn main() {
     type PKSim = PlayerKind<SimpleEval>;
     type PKBest = PlayerKind<GoodEval>;
 
+    print!("{}[2J", 27 as char); // clear terminal
     match input.trim() {
         "1" => run_game(PKSim::Human(Human::new('x')), PKSim::Human(Human::new('o'))),
 
@@ -46,7 +48,11 @@ fn main() {
             PKBest::Negamax(Negamax::new('o', 10, GoodEval {})),
         ),
 
+        "5" => run_game(
+            PKSim::Random(RandomAI::new('x')),
+            PKBest::Negamax(Negamax::new('o', 10, GoodEval {})),
+        ),
+
         _ => panic!("Invalid option"),
     };
-    print!("{}[2J", 27 as char); // clear terminal
 }
